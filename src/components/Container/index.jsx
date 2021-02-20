6087eda99c985dafde4497b359fc358cd85c092e
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-02-20 21:04:32 +0900

    feat: test first print

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 5px solid #764abc;
+  border-radius: 5px;
+  margin: 10px;
+  color: #764abc;
+  padding: 10px;
+`;
