a6527d6e62133a9fd22db788e9ab3e8c0e053193
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-02-22 23:33:11 +0900

    fix: add typography to landing

--- src/components/Typography/index.jsx
+++ src/components/Typography/index.jsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled, { withTheme } from "styled-components";
 
 const Typography = styled.div`
-  color: ${props => props.color ? props.color : 'white'};
+  color: ${props => props.color ? props.theme.colors[props.color] : 'white'};
   line-height: 1.5;
   white-space: pre-wrap;
   z-index: inherit;
@@ -20,14 +20,21 @@ const Typography = styled.div`
     `}
 
   ${props => 
+    props.landing &&
+    `
+      font-size: 6rem;
+      font-weigth: bold;  
+    `}
+
+  ${props =>
     props.large &&
     `
-      font-size: 8rem;
-      font-weigth: bold;  
+      font-size: 3rem;
+      font-weigth: 400;
     `}
 
   
 `;
 
-export default Typography;
+export default withTheme(Typography);
 
