5c60a35d0d2289bb56bb9657718a2e5102521805
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-02-25 00:17:53 +0900

    feat: add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Root from 'routes';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import theme from 'assets/theme/default';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <Route component={Root} />
+      </BrowserRouter>
     </React.StrictMode>
   </ThemeProvider>,
   document.getElementById('root')
