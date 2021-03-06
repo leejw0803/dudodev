fe4833098af1147146507aa3d6a4ef50728a85a5
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-03-07 00:46:15 +0900

    fix: remove TransitionGruop

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,22 +4,8 @@ import { GlobalStyle } from "components";
 import { createBrowserHistory } from "history";
 
 import { Landing, Programming, Hiphop, Fashion } from "pages";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const history = createBrowserHistory();
-
-const pageTrans = "trans";
-const classNames = {
-  appear: `${pageTrans} appear`,
-  appearActive: `${pageTrans} appear active`,
-  appearDone: `${pageTrans} appear done`,
-  enter: `${pageTrans} enter`,
-  enterActive: `${pageTrans} enter active`,
-  enterDone: `${pageTrans} enter done`,
-  exit: `${pageTrans} exit`,
-  exitActive: `${pageTrans} exit active`,
-  exitDone: `${pageTrans} exit done`,
-};
 
 function Root() {
   return (
@@ -27,30 +13,22 @@ function Root() {
       <GlobalStyle />
       <Route
         render={({ location }) => (
-          <TransitionGroup className="transitionGroup">
-            <CSSTransition
-              key={location.pathname}
-              classNames={classNames}
-              timeout={200}
-            >
-              <Switch location={location}>
-                <Route path="/" exact>
-                  <Landing />
-                </Route>
-                <Route path="/programming">
-                  <Programming />
-                </Route>
-                <Route path="/hiphop">
-                  <Hiphop />
-                </Route>
-                <Route path="/Fashion">
-                  <Fashion />
-                </Route>
+          <Switch location={location}>
+            <Route path="/" exact>
+              <Landing />
+            </Route>
+            <Route path="/programming">
+              <Programming />
+            </Route>
+            <Route path="/hiphop">
+              <Hiphop />
+            </Route>
+            <Route path="/Fashion">
+              <Fashion />
+            </Route>
 
-                <Redirect path="*" to="/" />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
+            <Redirect path="*" to="/" />
+          </Switch>
         )}
       />
     </Router>
