ff56c6aa5b27bd44b2cb482402115a99c3b16e56
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-08-19 21:53:57 +0900

    fix: remove unnecessary code

--- src/pages/Fashion/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-function Fashion() {
-  return <div>패션 페이지 입니다.</div>;
-}
-
-export default Fashion;
--- src/pages/Programming/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-function Programming() {
-  return <div>프로그래밍 페이지 입니다.</div>;
-}
-
-export default Programming;
