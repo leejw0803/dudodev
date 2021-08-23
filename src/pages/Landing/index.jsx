612b05a571603ba91bb54b938d9a452bdc016b52
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-08-23 23:39:40 +0900

    fix: fix landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,43 +1,16 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, { useState, useRef } from "react";
-import styled, { keyframes } from "styled-components";
-import { Margin, SideNav, Row } from "components";
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import { Margin, Row, Container, StyledRow, Layout } from "components";
 import Typography from "components/Typography";
-import { useHistory } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import allactions from "state";
+import { useSelector } from "react-redux";
 
-const shakeArrow = keyframes`
-  0% {
-    transform: translate3d(-2px, 0, 0);
-  }
-  50% {
-    transform: translate3d(2px, 0, 0);
-  }
-  100% {
-    transform: translate3d(-2px, 0, 0);
-  }
-`;
-
-const ArrowWrapper = styled.div`
-  animation: ${shakeArrow} 1s infinite linear alternate;
-`;
-
-const Container = styled.div`
+const StyledContainer = styled(Container)`
   margin: 0 30px;
-  display: flex;
-  flex-direction: column;
-  min-width: 50%;
-`;
-
-const StyledDiv = styled.div`
-  &:hover {
-    background-color: ${(props) => props.theme.colors.gray300};
-  }
 `;
 
 function Landing() {
-  const [stack, setStack] = useState(false);
+  const value = useSelector((state) => state.shared.scroll);
 
   const focusTarget = useRef([]);
 
@@ -52,11 +25,18 @@ function Landing() {
     });
   };
 
+  useEffect(() => {
+    scrollToRef(value);
+  }, [value]);
+
   return (
-    <>
-      <Container>
+    <Layout>
+      <StyledContainer>
         <div ref={(el) => (focusTarget.current[0] = el)}>
-          <Row align="center">
+          <Row>
+            <Margin size={9} />
+          </Row>
+          <StyledRow line={1}>
             <Typography large color="blue300">
               const{" "}
             </Typography>
@@ -69,52 +49,57 @@ function Landing() {
             <Typography large color="gray400">
               &#123;
             </Typography>
-          </Row>
-          <Row align="center">
-            <Margin row size={10} />
-            <Typography landing color="blue100">
+          </StyledRow>
+          <StyledRow line={2}>
+            <Margin row size={15} />
+            <Typography large color="blue100">
               name:{" "}
             </Typography>
-            <Typography landing color="blue100">
+            <Typography large color="blue100">
               leeJungWoo
             </Typography>
-            <Typography landing color="gray400">
+            <Typography large color="gray400">
               ,
             </Typography>
-          </Row>
-          <Row align="center">
-            <Margin row size={10} />
-            <Typography landing color="blue100">
+          </StyledRow>
+          <StyledRow line={3}>
+            <Margin row size={15} />
+            <Typography large color="blue100">
               nickName:{" "}
             </Typography>
-            <Typography landing color="blue100">
+            <Typography large color="blue100">
               dudo
             </Typography>
-            <Typography landing color="gray400">
+            <Typography large color="gray400">
               ,
             </Typography>
-          </Row>
-          <Row align="center">
-            <Margin row size={10} />
-            <Typography landing color="blue100">
+          </StyledRow>
+          <StyledRow line={4}>
+            <Margin row size={15} />
+            <Typography large color="blue100">
               motto:{" "}
             </Typography>
-            <Typography landing color="blue100">
+            <Typography large color="blue100">
               twblne
             </Typography>
-            <Typography landing color="gray400">
+            <Typography large color="gray400">
               ,
             </Typography>
-          </Row>
-          <Row>
+          </StyledRow>
+          <StyledRow line={5}>
             <Typography large color="gray400">
               &#125;;
             </Typography>
-          </Row>
+          </StyledRow>
         </div>
-        <Margin size={20} />
+        <StyledRow line={6}>
+          <Margin size={9} />
+        </StyledRow>
         <div ref={(el) => (focusTarget.current[1] = el)}>
-          <Row align="center">
+          <StyledRow line={7}>
+            <Margin size={9} />
+          </StyledRow>
+          <StyledRow line={8}>
             <Typography large color="blue300">
               const{" "}
             </Typography>
@@ -130,123 +115,97 @@ function Landing() {
             <Typography large color="blue300">
               =&gt;{" "}
             </Typography>
-            {!stack ? (
-              <>
-                <StyledDiv
-                  onClick={() => {
-                    setStack(true);
-                    scrollToRef("STACK");
-                  }}
-                >
-                  <Typography large color="gray400" pointer>
-                    &#123;...&#125;
-                  </Typography>
-                </StyledDiv>
-                <Margin row size={2} />
-                <Typography content1 color="coralPink300">
-                  <Row>
-                    <ArrowWrapper>👈🏼</ArrowWrapper> Click!!
-                  </Row>
-                </Typography>
-              </>
-            ) : (
-              <Typography large color="gray400" pointer>
-                &#123;
-              </Typography>
-            )}
-          </Row>
-          {stack && (
-            <>
-              <Row>
-                <Margin row size={10} />
-                <Typography large color="violet">
-                  return{" "}
-                </Typography>
-                <Typography large color="gray400">
-                  [
-                </Typography>
-              </Row>
-              <Row>
-                <Margin row size={20} />
-                <Typography large color="brown300">
-                  "JavaScript"
-                </Typography>
-                <Typography large color="gray400">
-                  ,{" "}
-                </Typography>
-                <Typography large color="brown300">
-                  "HTML"
-                </Typography>
-                <Typography large color="gray400">
-                  ,{" "}
-                </Typography>
-                <Typography large color="brown300">
-                  "CSS"
-                </Typography>
-                <Typography large color="gray400">
-                  ,
-                </Typography>
-                <Margin row size={15} />
-                <Typography large color="green300">
-                  // Level 2
-                </Typography>
-              </Row>
-              <Row>
-                <Margin row size={20} />
-                <Typography large color="brown300">
-                  "React.js"
-                </Typography>
-                <Typography large color="gray400">
-                  ,
-                </Typography>
-                <Typography large color="brown300">
-                  "styled-components"
-                </Typography>
-                <Typography large color="gray400">
-                  ,
-                </Typography>
-                <Margin row size={5} />
-                <Typography large color="green300">
-                  // Level 1
-                </Typography>
-              </Row>
-              <Row>
-                <Margin row size={20} />
-                <Typography large color="brown300">
-                  "Next.js"
-                </Typography>
-                <Typography large color="gray400">
-                  ,
-                </Typography>
-                <Typography large color="brown300">
-                  "TypeScript"
-                </Typography>
-                <Typography large color="gray400">
-                  ,
-                </Typography>
-                <Margin row size={31} />
-                <Typography large color="green300">
-                  // Level 0
-                </Typography>
-              </Row>
-              <Row>
-                <Margin row size={10} />
-                <Typography large color="gray400">
-                  ]
-                </Typography>
-              </Row>
-              <Row>
-                <Typography large color="gray400">
-                  &#125;;
-                </Typography>
-              </Row>
-            </>
-          )}
+            <Typography large color="gray400" pointer>
+              &#123;
+            </Typography>
+          </StyledRow>
+          <StyledRow line={9}>
+            <Margin row size={10} />
+            <Typography large color="violet">
+              return{" "}
+            </Typography>
+            <Typography large color="gray400">
+              [
+            </Typography>
+          </StyledRow>
+          <StyledRow line={10}>
+            <Margin row size={20} />
+            <Typography large color="brown300">
+              "JavaScript"
+            </Typography>
+            <Typography large color="gray400">
+              ,{" "}
+            </Typography>
+            <Typography large color="brown300">
+              "HTML"
+            </Typography>
+            <Typography large color="gray400">
+              ,{" "}
+            </Typography>
+            <Typography large color="brown300">
+              "CSS"
+            </Typography>
+            <Typography large color="gray400">
+              ,
+            </Typography>
+            <Margin row size={15} />
+            <Typography large color="green300">
+              // Level 2
+            </Typography>
+          </StyledRow>
+          <StyledRow line={11}>
+            <Margin row size={20} />
+            <Typography large color="brown300">
+              "React.js"
+            </Typography>
+            <Typography large color="gray400">
+              ,
+            </Typography>
+            <Typography large color="brown300">
+              "styled-components"
+            </Typography>
+            <Typography large color="gray400">
+              ,
+            </Typography>
+            <Margin row size={5} />
+            <Typography large color="green300">
+              // Level 1
+            </Typography>
+          </StyledRow>
+          <StyledRow line={12}>
+            <Margin row size={20} />
+            <Typography large color="brown300">
+              "Next.js"
+            </Typography>
+            <Typography large color="gray400">
+              ,
+            </Typography>
+            <Typography large color="brown300">
+              "TypeScript"
+            </Typography>
+            <Typography large color="gray400">
+              ,
+            </Typography>
+            <Margin row size={31} />
+            <Typography large color="green300">
+              // Level 0
+            </Typography>
+          </StyledRow>
+          <StyledRow line={13}>
+            <Margin row size={10} />
+            <Typography large color="gray400">
+              ];
+            </Typography>
+          </StyledRow>
+          <StyledRow line={14}>
+            <Typography large color="gray400">
+              &#125;;
+            </Typography>
+          </StyledRow>
         </div>
-
         <Margin size={100} />
-      </Container>
-    </>
+      </StyledContainer>
+    </Layout>
   );
 }
 
