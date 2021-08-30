79f68ca8cd804f2df09faccc200f6abd73e866b8
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-08-30 23:17:59 +0900

    fix: fix styled row margin size

--- src/components/StyledRow/index.jsx
+++ src/components/StyledRow/index.jsx
@@ -1,15 +1,25 @@
+import styled from "styled-components";
 import { Row, Col, Typography, Margin } from "components";
+
+const StaticCol = styled.div`
+  width: 60px;
+
+  @media (max-width: 768px) {
+    width: 20px;
+  }
+`;
 
 function StyledRow({ children, line }) {
   return (
     <Row align="center">
-      <Col size={1.5}>
+      <StaticCol>
         <Row justify="flex-end">
           <Typography title2 color="gray500">
             {line}
           </Typography>
         </Row>
-      </Col>
+      </StaticCol>
+      <Margin row size={5} />
       <Col size={11}>
         <Margin row size={2} />
         <Row>{children}</Row>
