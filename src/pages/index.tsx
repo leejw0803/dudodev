f11efa43412c23603e1e84cd650e771633361cf9
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2021-10-17 17:54:29 +0900

    feat: move contact info for user mode index to Footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -457,7 +457,7 @@ function Landing() {
             <Row>
               <Margin row size={5} />
               <Typography landing color="gray400">
-                Jung-woo LEE
+                이 정 우
               </Typography>
             </Row>
             <Col>
@@ -480,44 +480,6 @@ function Landing() {
                 </Typography>
               </Row>
             </Col>
-            <Margin size={10} />
-            <Row>
-              <Margin row size={5} />
-              <ContactBox>
-                <LogoForUser
-                  src="/assets/image/icn_github_simple_logo.png"
-                  alt="github logo"
-                  onClick={() => window.open('https://github.com/leejw0803')}
-                />
-                <LogoForUser
-                  src="/assets/image/icn_linkedin_simple_logo.png"
-                  alt="linkedin logo"
-                  onClick={() =>
-                    window.open(
-                      'https://www.linkedin.com/in/jung-woo-lee-871a091ba/',
-                    )
-                  }
-                />
-                <LogoForUser
-                  src="/assets/image/icn_email_logo.png"
-                  alt="email logo"
-                  onClick={() => window.open('mailto:dlwjdd@naver.com')}
-                />
-                <LogoForUser
-                  src="/assets/image/icn_rocketpunch_logo.svg"
-                  alt="rocketpunch logo"
-                  onClick={() =>
-                    window.open('https://www.rocketpunch.com/@dlfjstnl7')
-                  }
-                />
-                <LogoForUser
-                  src="/assets/image/icn_insta_logo.png"
-                  alt="insta logo"
-                  onClick={() => window.open('https://instagram.com/twblne')}
-                />
-              </ContactBox>
-              <Margin row size={5} />
-            </Row>
           </>
         )}
       </Container>
